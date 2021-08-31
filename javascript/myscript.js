21d31ebc5e7695d5f68901759f5445975edf5af3
gualtierigio <gualtierigio@gmail.com>
2021-08-31 15:16:37 +0200

    inizio esercizio morra

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,6 +1,6 @@
 
 
-let parolaUtente = prompt("inserire una parola palindroma");
+/**let parolaUtente = prompt("inserire una parola palindroma");
 
 function palindroma(parola){
     
@@ -14,4 +14,23 @@ if (parolaUtente == palindroma(parolaUtente)){
     console.log(parolaUtente + (" la parola è palindroma :D"));
 }else{
     console.log(parolaUtente + (" ah ah aha, non hai detto la parola magica!"));
+}*/
+
+function random5(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  let pcNumber = random5(1,6);
+
+console.log(pcNumber);
+
+function pariDispari(number){
+    if (number % 2 == 0){
+        return true;
+    }else {
+        return false
+    }
 }
+
