40b98c54e62ab94012e59ab0525e1178d24daf4e
gualtierigio <gualtierigio@gmail.com>
2021-08-31 16:10:23 +0200

    manca qualcosina

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -16,6 +16,9 @@ if (parolaUtente == palindroma(parolaUtente)){
     console.log(parolaUtente + (" ah ah aha, non hai detto la parola magica!"));
 }*/
 
+let utentePari = document.getElementById("pari");
+let utenteDispari =document.getElementById("dispari");
+
 function random5(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -36,9 +39,18 @@ let numeroUtente = prompt("inserisci un numero da 1 a 5");
 
 if (numeroUtente < 1 || numeroUtente > 5){
     alert("giochi corretto cortesemente, ora scelgo io")
-    numeroUtente = console.log(random5(1,5));
-}else if (numeroUtente != Number){
+    numeroUtente = console.log(random5(1,6));
+}else if (numeroUtente == NaN){
     alert("i numeri sono numeri, ora scelgo io")
-    numeroUtente = console.log(random5(1,5));
+    numeroUtente = console.log(random5(1,6));
 }
 
+let result = pcNumber + numeroUtente;
+
+console.log(result);
+
+if (utentePari == true && pariDispari(result) == true || utenteDispari == false && pariDispari(result) == false){
+    console.log("utente vince");
+}else {
+    console.log("vince il banco, mi spiace");
+}
